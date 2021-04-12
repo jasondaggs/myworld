7a7931c1f4d0d7a85d19dd1558bec145aef97c4e
jasondaggs <jason.daggs@gmail.com>
2021-04-12 01:12:51 +0000

    Final changes

--- script/myplugin.cpp
+++ script/myplugin.cpp
@@ -2,16 +2,18 @@
 
 namespace gazebo
 {
-  class WorldPluginMyRobot : public WorldPlugin
+  class WorldPluginMyPlugin : public WorldPlugin
   {
-    public: WorldPluginMyRobot() : WorldPlugin()
-            {
-              printf("Hello World!\n");
-            }
+
+    public: WorldPluginMyPlugin() : WorldPlugin()
+    {
+              printf("Welcome to Jasons World!\n");
+    }
 
     public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
-            {
-            }
+    {
+    }
   };
-  GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
+
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyPlugin)
 }
--- script/run_gazebo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:/home/workspace/myworld/build
-
-gazebo ../worlds/myworld.sdf &
